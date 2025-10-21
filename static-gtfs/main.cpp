8f69f95a034530c8aa9746566666a893f552ee90
JMu <64600882+JettM9104@users.noreply.github.com>
2025-10-21 08:35:58 -0400

    Add calendarDate comparison and arithmetic operators
    Implemented comparison operators (<, >, <=, >=, ==), a printInfo method, and an addition operator for the calendarDate class to support date arithmetic and comparisons. These enhancements improve date handling and debugging capabilities.

--- static-gtfs/main.cpp
+++ static-gtfs/main.cpp
@@ -236,6 +236,7 @@ public:
 
 };
 
+
 class calendarDate {
 public:
     int year;
@@ -249,7 +250,134 @@ public:
         this->day = day;
     }
 
+    void printInfo() {
+        cout << "year: " << year << std::endl;
+        cout << "month: " << month << std::endl;
+        cout << "day: " << day << std::endl;
+    }
+
+    bool operator< (const calendarDate& other) {
+        if (other.year > this->year) {
+            return true;
+        } else if (other.year < this->year) {
+            return false;
+        } else { // year is equal
+            if (other.month > this->month) {
+                return true;
+            } else if (other.month < this->month) {
+                return false;
+            } else { // months are equal
+                if (other.day > this->day) {
+                    return true;
+                } else if (other.day > this->day) {
+                    return false;
+                } else { // days are equal
+                    return false;
+                }
+            }
+        }
+    }
+
+    bool operator> (const calendarDate& other) {
+        if (other.year < this->year) {
+            return true;
+        } else if (other.year > this->year) {
+            return false;
+        } else { // year is equal
+            if (other.month < this->month) {
+                return true;
+            } else if (other.month > this->month) {
+                return false;
+            } else { // months are equal
+                if (other.day < this->day) {
+                    return true;
+                } else if (other.day > this->day) {
+                    return false;
+                } else { // days are equal
+                    return false;
+                }
+            }
+        }
+    }
+
+    bool operator<= (const calendarDate& other) {
+        if (other.year > this->year) {
+            return true;
+        } else if (other.year < this->year) {
+            return false;
+        } else { // year is equal
+            if (other.month > this->month) {
+                return true;
+            } else if (other.month < this->month) {
+                return false;
+            } else { // months are equal
+                if (other.day > this->day) {
+                    return true;
+                } else if (other.day > this->day) {
+                    return false;
+                } else { // days are equal
+                    return true;
+                }
+            }
+        }
+    }
+
+    bool operator>= (const calendarDate& other) {
+        if (other.year < this->year) {
+            return true;
+        } else if (other.year > this->year) {
+            return false;
+        } else { // year is equal
+            if (other.month < this->month) {
+                return true;
+            } else if (other.month > this->month) {
+                return false;
+            } else { // months are equal
+                if (other.day < this->day) {
+                    return true;
+                } else if (other.day > this->day) {
+                    return false;
+                } else { // days are equal
+                    return true;
+                }
+            }
+        }
+    }
+
+    bool operator== (const calendarDate& other) {
+        return (other.year == this->year && other.month == this->month && other.day == this->day);
+    }
+
+    calendarDate operator+ (const calendarDate& other) {
+        calendarDate output;
+
+        int regularDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int leapDays[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        output.year = this->year + other.year;
+        output.month = this->month + other.month;
+        output.day = this->day + other.day;
+
+        if (output.month > 12) {
+            output.year += 1;
+            output.month -= 12;
+        }
+
+        if ((output.year % 4 == 0 && output.year % 100 != 0 )|| output.year % 400 == 0) {
+            if (output.day > leapDays[output.month - 1]) {
+                output.day -= leapDays[output.month - 1];
+                output.month++;
+            }
+        } else {
+            if (output.day > regularDays[output.month - 1]) {
+                output.day -= regularDays[output.month - 1];
+                output.month++;
+            }
+        }
+        return output;
+    }
 };
+
 
 std::vector<string> parseDataCSV(const string& input);
 std::unordered_map<string, int> createMapFromVector(std::vector<string> param);
