97987003bac35ae6c4f4903210219744062cbca2
JMu <64600882+JettM9104@users.noreply.github.com>
2025-10-28 13:03:33 -0400

    Fix formatting by adding a newline at the end of varadic.cpp

--- other-testing/varadic.cpp
+++ other-testing/varadic.cpp
@@ -8,6 +8,7 @@ double average(Args... args) {
     return sum / sizeof...(args);
 }
 
+
 int main() {
     std::cout << average(1, 2, 3, 4, 5) << std::endl;        // Output: 3
     std::cout << average(2.5, 3.5, 4.5) << std::endl;        // Output: 3.5
