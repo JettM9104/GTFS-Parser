f5442bb9cdc56c20251cb72fd2bcd96c87c92a5b
JMu <jettmuu@gmail.com>
2026-08-20 22:47:22 -0400

    Add legacy getTrip benchmark tool
    Adds `fast-static-gtfs/gettriptestold.cpp`, an interactive C++ test utility that prints trip JSON (stops and shape markers) and measures execution time per query. Also updates `.gitignore` to exclude the compiled `fast-static-gtfs/gettriptestold` binary.

---
+++ fast-static-gtfs/gettriptestold.cpp
@@ -0,0 +1,96 @@
+#include "httplib.h"
+#include <iostream>
+#include <string>
+#include "../static-gtfs/gtfs.hpp"
+using namespace httplib;
+
+// --- Your C++ functions ---
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <cstdlib>
+#include <chrono>
+
+using std::cout;
+
+void getTrip(const string& trip_id, const int& precision = 6) {
+
+    std::cout << std::fixed << std::setprecision(precision);
+
+
+    std::vector<gtfs::trip_segment> tripSegments = gtfs::getAllStops(trip_id);
+    gtfs::trip tx = gtfs::getTripInfo(trip_id);
+    gtfs::route bx = gtfs::getRouteInfo(tx.route_id);
+    std::vector<gtfs::shape> tsx = gtfs::getShapeInfo(tx.shape_id);
+
+    std::vector<gtfs::stop> stops;
+
+    for (gtfs::trip_segment& x : tripSegments) {
+        stops.push_back(gtfs::getStopInfo(x.stop.stop_id));
+    }
+    int length = stops.size();
+
+    std::cout << "{\n";
+    std::cout << "\t\"total\": " << length << ",\n";
+    std::cout << "\t\"trip_id\": " << tx.trip_id << ",\n";
+    std::cout << "\t\"route_id\": " << tx.route_id << ",\n";
+    std::cout << "\t\"route_short_name\": \"" << bx.route_short_name << "\",\n";
+    std::cout << "\t\"route_long_name\": \"" << bx.route_long_name << "\",\n";
+    std::cout << "\t\"route_color\": \"#" << bx.route_color << "\",\n";
+
+    std::cout << "\t\"stops\": [\n";
+    for (int i = 0; i < length; i++) {
+        gtfs::stop x = stops[i];
+        gtfs::trip_segment y = tripSegments[i];
+        std::cout << "\t\t{ \"lat\": " << x.stop_lat <<
+                ", \"lng\": " << x.stop_lon <<
+                ", \"code\": \""<< x.stop_code <<
+                "\", \"id\": \"" << x.stop_id <<
+                "\", \"name\": \"" << x.stop_name <<
+                "\", \"time\": \"" << y.stop.arrival_time.leadingRoundedTime() << "\"" <<
+                ", \"stop_sequence\": " << y.stop.stop_sequence <<
+                (i == (length - 1) ? " }\n" : " },\n");
+    }
+
+
+    cout << "\t],\n";
+
+    int tx_length = tsx.size();
+
+    std::cout << "\t\"pos_markers\": [\n";
+    for (int i = 0; i < tx_length; i++) {
+        gtfs::shape x = tsx[i];
+        std::cout << "\t\t{ \"lat\": " << x.shape_pt_lat <<
+                ", \"lng\": " << x.shape_pt_lon <<
+                ", \"sequence\": " << x.shape_pt_sequence <<
+                (i == (tx_length - 1) ? " }\n" : " },\n");
+    }
+
+    cout << "\t]\n}\n";
+
+
+}
+
+
+int main() {
+    cout << "done init\n";
+
+    while (true) {
+        string a;
+        std::cin >> a;
+
+        auto start = std::chrono::steady_clock::now();
+        getTrip(a);
+        std::cout << "done\n";
+
+        auto end = std::chrono::steady_clock::now();
+
+        auto elapsed = end - start;
+        // 4. Convert and print the duration in various units
+        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count()
+        << " ms\n" << std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count()
+        << " µs\n";
+    }
+    return 0;
+}
